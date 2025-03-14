712c2642995b109a9dc66e180128ee17d2a61e14
Caroline Costa <xcarol.costa@outlook.com>
2025-03-14 14:51:47 -0300

    implementando função atualizar lista/ implementando evento ao botão

--- app.js
+++ app.js
@@ -4,11 +4,29 @@ let amigos = []
 function adicionarAmigo(){
   let amigo = document.getElementById("amigo").value
   
-  if(amigo){
-    amigos.push(amigo)
-    document.getElementById("amigo").value = ''
+  if(amigo === ""){
+    return alert(`Por favor, insira um nome`) 
   } else {
-    alert(`Por favor, insira um nome`) 
+    amigos.push(amigo)
+     document.getElementById("amigo").value = '';
+     atualizarLista()
   }
-   
 }
+
+
+function atualizarLista(){
+  const lista = document.getElementById("listaAmigos")
+    lista.innerHTML = '';
+
+  for (const amigo of amigos) {
+    const li = document.createElement('li')
+    li.textContent = amigo;
+    lista.appendChild(li);
+  }
+}
+
+
+const button = document.getElementsByClassName("button-add")[0]
+
+button.addEventListener('click', adicionarAmigo)
+
