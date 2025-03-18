b301da0adce176bcce68e3078289f4d21c999290
Caroline Costa <xcarol.costa@outlook.com>
2025-03-18 18:30:15 -0300

    implementando função sortear amigo

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos = []
+let amigos = [];
 
-function adicionarAmigo(){
-  let amigo = document.getElementById("amigo").value
-  
-  if(amigo === ""){
-    return alert(`Por favor, insira um nome`) 
-  } else {
-    amigos.push(amigo)
-     document.getElementById("amigo").value = '';
-     atualizarLista()
-  }
+function adicionarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+
+    if (amigo === "") {
+        return alert("Por favor, insira um nome");
+    } else {
+        amigos.push(amigo);
+        document.getElementById("amigo").value = "";
+        atualizarLista();
+    }
 }
 
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
 
-function atualizarLista(){
-  const lista = document.getElementById("listaAmigos")
-    lista.innerHTML = '';
-
-  for (const amigo of amigos) {
-    const li = document.createElement('li')
-    li.textContent = amigo;
-    lista.appendChild(li);
-  }
+    for (const amigo of amigos) {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
 }
 
+const botaoAdicionar = document.getElementsByClassName("button-add")[0];
+botaoAdicionar.addEventListener("click", adicionarAmigo);
 
-const button = document.getElementsByClassName("button-add")[0]
+function sortearAmigo() {
+    if (!amigos.length) {
+        return alert("Não há amigos para o sorteio");
+    }
 
-button.addEventListener('click', adicionarAmigo)
+    const numeroSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[numeroSorteado];
 
+    const resultado = document.getElementById("resultado");
+    const li = document.createElement("li");
+    li.textContent = amigoSorteado;
+    resultado.appendChild(li);
+
+    amigos = [];
+    atualizarLista();
+}
+
+const botaoSortear = document.getElementsByClassName("button-draw")[0];
+botaoSortear.addEventListener("click", sortearAmigo);
